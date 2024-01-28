85b60298e9ddbbc50ff5dba2d676c42be6c1da53
Ethorax <ezeldagamer@gmail.com>
2024-01-28 13:20:43 -0500

    Something

--- Assets/Scripts/bottle.cs
+++ Assets/Scripts/bottle.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class bottle : MonoBehaviour
 {
+
+    public float speed = 0.5f;
+    public float rSpeed = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class bottle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = new Quaternion.Euler(0,0,transform.rotation.z + rSpeed);
     }
 }
